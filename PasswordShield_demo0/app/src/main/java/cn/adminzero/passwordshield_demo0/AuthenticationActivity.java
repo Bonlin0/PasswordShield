a6fb798b9c059a18e78ced4b1cd93864ed528881
Hell05 <32389641+Bonlin0@users.noreply.github.com>
2019-07-13 18:13:26 +0800

    FingerPrint Auth 1.0

--- PasswordShield_demo0/app/src/main/java/cn/adminzero/passwordshield_demo0/AuthenticationActivity.java
+++ PasswordShield_demo0/app/src/main/java/cn/adminzero/passwordshield_demo0/AuthenticationActivity.java
@@ -3,13 +3,28 @@ package cn.adminzero.passwordshield_demo0;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import cn.adminzero.passwordshield_demo0.biometriclib.BiometricPromptManager;
 
 //TODO Font color of Tool bar is black
 
 public class AuthenticationActivity extends AppCompatActivity {
+
+    private BiometricPromptManager fingerprintManager;
+    private Button unlockWithFingerprintButton;
+    private EditText master_password_edit;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +32,11 @@ public class AuthenticationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_authentication);
         Toolbar toolbar = findViewById(R.id.toolbar_authentication);
         setSupportActionBar(toolbar);
+        //设置指纹识别相关
+        unlockWithFingerprintButton = findViewById(R.id.unlock_with_fingerprint_button);
+        fingerprintManager = BiometricPromptManager.from(this);
+        master_password_edit = findViewById(R.id.master_password_edit);
+
     }
 
     public void onClickConfirm(View view){
@@ -35,10 +55,44 @@ public class AuthenticationActivity extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
-    //TODO 将使用人脸解锁的按钮方法改为正确的方法
-    public void onClickUnlockWithFingerPrint(View view){
-        Intent intent = new Intent(this, fingerActivity.class);
-        startActivity(intent);
-        finish();
+
+    public void onClickUnlockWithFingerPrint(View view) {
+        if (fingerprintManager.isBiometricPromptEnable()) {
+            fingerprintManager.authenticate(new BiometricPromptManager.OnBiometricIdentifyCallback() {
+                @Override
+                public void onUsePassword() {
+                    //使得密码输入框获得焦点
+                    master_password_edit.requestFocus();
+                    AuthenticationActivity.this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+                }
+                //成功识别后
+                @Override
+                public void onSucceeded() {
+                    Toast.makeText(AuthenticationActivity.this, "onSucceeded", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(AuthenticationActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+                @Override
+                public void onFailed() {
+                    //验证失败后弹出提示
+                    Toast.makeText(AuthenticationActivity.this, "onFailed", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onError(int code, String reason) {
+                    //验证出现错误时弹出提示
+                    Toast.makeText(AuthenticationActivity.this, "onError", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCancel() {
+                    //用户取消
+                }
+            });
+        }
+
     }
+
 }
--- PasswordShield_demo0/app/src/main/java/cn/adminzero/passwordshield_demo0/SettingsActivity.java
+++ PasswordShield_demo0/app/src/main/java/cn/adminzero/passwordshield_demo0/SettingsActivity.java
@@ -1,13 +1,28 @@
 package cn.adminzero.passwordshield_demo0;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragment;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.PreferenceScreen;
+
+import cn.adminzero.passwordshield_demo0.biometriclib.BiometricPromptManager;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Switch unlock_with_fingerprint_switch_preference;
+    private BiometricPromptManager fingerprintManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +39,16 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        //使用指纹识别管理器
+        fingerprintManager = BiometricPromptManager.from(this);
+        //查找switch控件
+        unlock_with_fingerprint_switch_preference = findViewById
+                (R.id.unlock_with_fingerprint_switch_preference);
+        if (unlock_with_fingerprint_switch_preference == null) {
+            Toast.makeText(SettingsActivity.this, "onFailed", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -33,3 +58,90 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 }
+
+    /*
+    private class onChangeUnlockWithFingerPrintListener implements PreferenceManager.OnPreferenceTreeClickListener {
+        public boolean onPreferenceTreeClick(Preference preference){
+
+            if(unlockWithFingerprintSwitch.isChecked()){
+                Toast.makeText(SettingsActivity.this, "onFailed", Toast.LENGTH_SHORT).show();
+            }
+
+            if (fingerprintManager.isBiometricPromptEnable()) {
+                fingerprintManager.authenticate(new BiometricPromptManager.OnBiometricIdentifyCallback() {
+                    @Override
+                    public void onUsePassword() {
+
+                    }
+                    //成功识别后
+                    @Override
+                    public void onSucceeded() {
+                        unlockWithFingerprintSwitch.setChecked(true);
+                    }
+
+                    @Override
+                    public void onFailed() {
+                        //验证失败后弹出提示
+                        Toast.makeText(SettingsActivity.this, "onFailed", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(int code, String reason) {
+                        //验证出现错误时弹出提示
+                        Toast.makeText(SettingsActivity.this, "onError", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        //用户取消
+                    }
+
+                });
+            }
+        }
+    }
+*/
+
+
+/*
+    public void onChangeUnlockWithFingerPrint(final View view){
+        final Switch unlockWithFingerprintSwitch = (Switch)view;
+        if(unlockWithFingerprintSwitch.isChecked()){
+            Toast.makeText(SettingsActivity.this, "onFailed", Toast.LENGTH_SHORT).show();
+        }
+
+        if (fingerprintManager.isBiometricPromptEnable()) {
+            fingerprintManager.authenticate(new BiometricPromptManager.OnBiometricIdentifyCallback() {
+                @Override
+                public void onUsePassword() {
+
+                }
+                //成功识别后
+                @Override
+                public void onSucceeded() {
+                    unlockWithFingerprintSwitch.setChecked(true);
+                }
+
+                @Override
+                public void onFailed() {
+                    //验证失败后弹出提示
+                    Toast.makeText(SettingsActivity.this, "onFailed", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onError(int code, String reason) {
+                    //验证出现错误时弹出提示
+                    Toast.makeText(SettingsActivity.this, "onError", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onCancel() {
+                    //用户取消
+                }
+
+            });
+        }
+    }
+}
+
+*/
